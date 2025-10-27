c556b699bf011dc2a8d4d859f59e2d263b687f66
Toma Andreev <tomaanreev12@gmail.com>
2025-10-27 14:53:30 +0200

    Create IPropertyService and methods for implement

---
+++ RealEstateSearcher.Infrastructure/Dtos/PropertyDto.cs
@@ -0,0 +1,13 @@
+﻿namespace RealEstateSearcher.Infrastructure.Dtos
+{
+    public class PropertyDto
+    {
+        public string? Title { get; set; }
+        public string Quarter { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Area { get; set; }
+        public int Floor { get; set; }
+        public int TotalFloors { get; set; }
+        public string? BuildingType { get; set; }
+    }
+}
